f367badc41c324197678f46147c5e83a76328d08
Viktor Farcic <viktor@farcic.com>
2023-05-30 01:32:53 +0200

    Civo

--- cdk8s/hacks/schemahero.sh
+++ cdk8s/hacks/schemahero.sh
@@ -2,5 +2,3 @@
 cat dist/civo-all.k8s.yaml hacks/schemahero.yaml hacks/write-connection-secrets-to-namespace.yaml > dist/civo-all.k8s.yaml.tmp
 
 mv dist/civo-all.k8s.yaml.tmp dist/civo-all.k8s.yaml
-
-rm dist/civo-all.k8s.yaml.tmp
